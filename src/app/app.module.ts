ee987476b8e7068fe6ac1fdd0d6affa6fd179af1
Asif Ekbal <asif.ekbal@peoplestrong.com>
2019-06-27 20:34:02 +0530

    Angular Dependencies issues

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import 'hammerjs';
 import { AppComponent } from './app.component';
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, BrowserAnimationsModule, MatToolbarModule, FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
