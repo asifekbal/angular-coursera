ef7dff3c274e2d2dbe6e0b77b116e50777f757cf
Asif Ekbal <asif.sunny678@gmail.com>
2019-10-06 13:31:37 +0530

    RxJS Part 2

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -12,6 +12,9 @@ import { Dish } from '../shared/dish';
 export class DishdetailComponent implements OnInit {
 
     dish: Dish;
+    dishIds: string[];
+    prev: string;
+    next: string;
     constructor(
         private route: ActivatedRoute,
         private dishService: DishService,
@@ -21,8 +24,17 @@ export class DishdetailComponent implements OnInit {
     }
 
     ngOnInit() {
+        this.dishService.getDishIds().subscribe((dishIds) => this.dishIds = dishIds);
         let id = this.route.snapshot.params['id'];
-        this.dishService.getDish(id).subscribe((dish) => this.dish = dish);
+        this.dishService.getDish(id).subscribe((dish) => {
+            this.dish = dish;
+            this.setPrevNext(dish.id);
+        });
+    }
+    setPrevNext(dishId: string) {
+        const index = this.dishIds.indexOf(dishId);
+        this.prev = this.dishIds[(this.dishIds.length + index - 1) % this.dishIds.length];
+        this.next = this.dishIds[(this.dishIds.length + index + 1) % this.dishIds.length];
     }
     goBack() {
         this.location.back();
